ad157e1961100e0aa4a550e556aec70fe283e32e
gerprog - Gerard Daly <ger_daly@yahoo.com>
2014-04-19 16:23:51 +0100

    added salon controller

---
+++ salonMVC/Controllers/SalonController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace salonMVC.Controllers
+{
+    public class SalonController : Controller
+    {
+        //
+        // GET: /Salon/
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //
+        // GET: /Salon/Browse?treatment=Hairdye
+        public string Browse(string treatment)
+        {
+            string message = HttpUtility.HtmlEncode("Salon.Browse, Treatment = " + treatment);
+
+            return message;
+        }
+        //
+        // GET: /Salon/Details
+        public string Details(int id)
+        {
+            string message = "Salon.Details, ID = " + id;
+            return message;
+        }
+    }
+}
